527b58b5d02bd1f6e852381bc9e539b81ade394c
Pravinkumar Putta <pravinkumarp@mkcl.org>
2021-05-26 20:42:21 +0530

    Initial commit

---
+++ app/src/main/java/com/pravinkumarp/mvpdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pravinkumarp.mvpdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
